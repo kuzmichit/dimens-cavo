9211db82153e892f21b639f4200415feb9cdb703
kuzmichit <kuzmichit@gmail.com>
2024-04-10 13:38:57 +0200

    commit save point

--- src/components/parametri_iniziali/parametri_iniziali.js
+++ src/components/parametri_iniziali/parametri_iniziali.js
@@ -5,15 +5,28 @@ import Form from 'react-bootstrap/Form';
 const ParametriIniziali = () => {
 	
   return (
-    <> 
-      <Form.Group className = "mb-3" controlId = "formBasicEmail">
-        <Form.Label htmlFor = "inputPardiAlimentazione">Parametri di alimentazione</Form.Label>
-        <Form.Control type = "input" placeholder = "La tensione di linea" />
-        <Form.Text id = "ULinea">
-       
-        </Form.Text> 
+    <Form className = 'ms-5' id = 'parametri_iniziali'> 
+      <Form.Group className = "mb-3 mt-5 w-75">
+        <Form.Label className = 'ps-2 '>Tensione di linea (volt)</Form.Label>
+        <Form.Select aria-label = "La tensione di linea (volts)" id = 'Unom'>
+          <option value = "230">230 V</option>
+          <option value = "400">400 V</option>
+        </Form.Select>
+        <span>hello</span>
       </Form.Group>
-    </>
+      <Form.Group className = "mb-3 mt-1 w-75" controlId = "">
+        <Form.Label className = 'ps-2 '>Lunghezza di linea (m)</Form.Label>
+        <Form.Control type = "input" placeholder = "10" />
+      </Form.Group>
+      <Form.Group className = "mb-3 mt-1 w-75" controlId = "">
+        <Form.Label className = 'ps-2 '>Potenza elettrica del carico (watt)</Form.Label>
+        <Form.Control type = "input" placeholder = "100" />
+      </Form.Group>
+      <Form.Group className = "mb-3 mt-1 w-75" controlId = "">
+        <Form.Label className = 'ps-2 '>Caduta di tensione ammissibile (%)</Form.Label>
+        <Form.Control type = "input" defaultValue = '4' />
+      </Form.Group>
+    </Form>
   );
 }
 
